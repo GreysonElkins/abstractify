9ce7a821c776305dff93b79a57e7b614f0820f03
Greyson Elkins <greysonelkins@gmail.com>
2020-09-16 20:21:02 -0600

    Add buttons to Header #8

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return <h1>hello header</h1>
+  return (
+    <header>
+      <NavLink exact to='/'>Home</NavLink>
+      <button>About</button>
+      <button>Refresh</button>
+      <button>Save</button>
+      <NavLink to='/your-sets'>Your Sets</NavLink>
+    </header>
+  )
 }
 
 export default Header
