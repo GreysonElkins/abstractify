22b94c882d0440f0b47d5b4d4f4f49376f88f583
Greyson Elkins <greysonelkins@gmail.com>
2020-09-16 19:27:55 -0600

    Write fetch get for 80 images #11

--- src/ApiHelper/ApiHelper.js
+++ src/ApiHelper/ApiHelper.js
@@ -1,1 +1,22 @@
 require("dotenv").config();
+
+export const getPhotos = () => {
+  let res
+  try {
+    return fetch('https://api.pexels.com/v1/curated?per_page=80', {
+      authorization: API_KEY
+    }).then(response => {
+      let res = response
+      return response.json() 
+    }).then(imageSet => {
+      if (res.ok) {
+        return imageSet
+      } else {
+        return `Something went wrong while
+         getting the images, error: ${response.status}`
+      }
+    })
+  } catch (error) {
+    return error.message
+  }
+}
