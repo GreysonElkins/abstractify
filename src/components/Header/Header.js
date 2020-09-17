6ec3ba73002c8aa4e53a0da206fe8bc2ed1c3eb5
Greyson Elkins <greysonelkins@gmail.com>
2020-09-16 21:14:27 -0600

    Begin styling / convert Header to class component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import './Header.scss'
 
-const Header = () => {
-  return (
-    <header>
-      <NavLink exact to='/'>Home</NavLink>
-      <button>About</button>
-      <button>Refresh</button>
-      <button>Save</button>
-      <NavLink to='/your-sets'>Your Sets</NavLink>
-    </header>
-  )
+class Header extends Component {
+  constructor() {
+    super() 
+      this.state = {
+        title: ['^','3','5','/','4','^','[','/','!','"','v']
+      }
+  }
+
+  render() {
+    return (
+      <header>
+        <Link to="/" className="title">
+          <span className="title" tabIndex="0">
+            {this.state.title.join('')}
+          </span>
+        </Link>
+        <nav>
+          <NavLink
+            exact
+            to="/"
+            className="inactive-link"
+            activeClassName="active-link"
+          >
+            Home
+          </NavLink>
+          <button title="About Abstractify">About</button>
+          <button title="Refresh image set">Refresh</button>
+          <button title="Save this image set">Save</button>
+          <NavLink
+            to="/your-sets"
+            className="inactive-link"
+            title="See your saved image sets"
+            activeClassName="active-link"
+          >
+            Your Sets
+          </NavLink>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header
