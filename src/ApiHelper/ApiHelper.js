91f2c03a5d00d41fcb09e351e8e0dfe14893f4a2
Greyson Elkins <greysonelkins@gmail.com>
2020-09-16 19:43:41 -0600

    Clean data in Api #11

--- src/ApiHelper/ApiHelper.js
+++ src/ApiHelper/ApiHelper.js
@@ -1,16 +1,35 @@
 require("dotenv").config();
 
+const pages = ['https://api.pexels.com/v1/curated?per_page=80']
+
+const cleanPhotos = (photos) => {
+  return photos.map(photo => {
+    return {
+      id: photo.id,
+      width: photo.width,
+      height: photo.height,
+      url: photo.url,
+      photographer: {
+        name: photo.photographer,
+        url: photo.photographer_url,
+      },
+      locked: false 
+    };
+  })
+}
+
 export const getPhotos = () => {
   let res
   try {
-    return fetch('https://api.pexels.com/v1/curated?per_page=80', {
+    return fetch(pages[0], {
       authorization: API_KEY
     }).then(response => {
       let res = response
       return response.json() 
     }).then(imageSet => {
       if (res.ok) {
-        return imageSet
+        pages.unshift(imageSet.nextPage)
+        return cleanPhotos(imageSet.photos)
       } else {
         return `Something went wrong while
          getting the images, error: ${response.status}`
