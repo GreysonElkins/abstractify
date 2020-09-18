352d54d31a3632f309b4d511800920740996ffc8
Greyson Elkins <greysonelkins@gmail.com>
2020-09-17 22:01:23 -0600

    Relocate glitch / title to App #22

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,68 +12,29 @@ import './App.scss';
 
 class App extends Component {
   constructor() {
-    super() 
+    super();
     this.state = {
+      title: [],
       foreignSet: [],
       savedSets: [],
-      popUpTrigger: {show: ''}
-    }
-  }
-
-  componentDidMount() {
-    // this.refreshForeignSet()
-    this.setState({foreignSet: response})
-  }
-  
-  refreshForeignSet = () => {
-    this.markLoadedImagesSeen() 
-    if (this.checkQuantityUnseen() < 20) {
-      getImages().then(images => {
-        this.setState({ foreignSet: images })
-      })
-    }
-  }
-
-  toggleImageLock = (id) => {
-    const update = this.state.foreignSet.map(img => {
-      if (img.id === id) {
-        img.locked = img.locked ? false : true
-      }
-      return img
-    })
-    this.setState({ foreignSet: update })
-  }
-
-  checkQuantityUnseen = () => {
-    return this.state.foreignSet.reduce((unseenQty, img) => {
-      if (!img.seen) unseenQty++
-      return unseenQty
-    }, 0)
-  }
-
-  markLoadedImagesSeen = () => {
-    const loaded_imgs = document.querySelectorAll('img')
-    const matchIds = []
-    loaded_imgs.forEach(node => {
-      matchIds.push(node.id)
-    })
-
-    const updateSet = this.state.foreignSet.map(img => {  
-      if (matchIds.includes(img.id.toString()) && !img.locked) {
-        img.seen = true
-      }
-      return img
-    })
-    this.setState({ foreignSet: updateSet })
+      popUpTrigger: { show: "" },
+      isGaudy: true,
+    };
   }
 
   render() {
     return (
       <main>
-        <Header refresh={this.refreshForeignSet} />
+        <Header 
+          title={this.state.title} 
+          refresh={this.refreshForeignSet} 
+          isGaudy={this.state.isGaudy}
+          toggleGaudy={this.toggleGaudy}
+          glitch={this.glitchLetter}
+        />
         <Route exact path="/">
-          <MainPage 
-            images={this.state.foreignSet} 
+          <MainPage
+            images={this.state.foreignSet}
             toggleImageLock={this.toggleImageLock}
           />
         </Route>
@@ -88,7 +49,118 @@ class App extends Component {
       </main>
     );
   }
+
+  componentDidMount() {
+    this.loadTitle();
+    // this.refreshForeignSet()
+    this.setState({ foreignSet: response });
+  }
+
+  refreshForeignSet = () => {
+    this.markLoadedImagesSeen();
+    if (this.checkQuantityUnseen() < 20) {
+      getImages().then((images) => {
+        this.setState({ foreignSet: images });
+      });
+    }
+    this.glitchLetter(true);
+  };
+
+  checkQuantityUnseen = () => {
+    return this.state.foreignSet.reduce((unseenQty, img) => {
+      if (!img.seen) unseenQty++;
+      return unseenQty;
+    }, 0);
+  };
+
+  markLoadedImagesSeen = () => {
+    const loaded_imgs = document.querySelectorAll("img");
+    const matchIds = [];
+    loaded_imgs.forEach((node) => {
+      matchIds.push(node.id);
+    });
+
+    const updateSet = this.state.foreignSet.map((img) => {
+      if (matchIds.includes(img.id.toString()) && !img.locked) {
+        img.seen = true;
+      }
+      return img;
+    });
+    this.setState({ foreignSet: updateSet });
+  };
+
+  toggleImageLock = (id) => {
+    const update = this.state.foreignSet.map((img) => {
+      if (img.id === id) {
+        img.locked = img.locked ? false : true;
+      }
+      return img;
+    });
+    this.setState({ foreignSet: update });
+    this.glitchLetter(true);
+  };
+
+  loadTitle = () => {
+    for (let i = 0; i < 11; i++) {
+      this.glitchLetter(false, i);
+      for (let j = 0; j < this.randomIndex(4); j++) {
+        setTimeout(() => {
+          this.glitchLetter(false, i);
+          this.glitchLetter(true, i);
+        }, this.randomInterval(j === 0 ? 1000 : undefined));
+      }
+    }
+  };
+
+  glitchLetter = (fix, index = this.randomIndex(11)) => {
+    const glitchTitle = this.state.title;
+    const letterLimit = () => {
+      return letterOptions[index.toString()].length;
+    };
+    glitchTitle[parseInt(index)] =
+      letterOptions[index.toString()][this.randomIndex(letterLimit())];
+    this.setState({ title: glitchTitle });
+    if (fix) this.correctLetter(index);
+  };
+
+  correctLetter = (index) => {
+    setTimeout(() => {
+      const fixTitle = this.state.title;
+      fixTitle[parseInt(index)] = letterOptions[index][0];
+      this.setState({ title: fixTitle });
+    }, this.randomInterval());
+  };
+
+  randomIndex = (limit) => {
+    let index = Math.floor(Math.random() * (limit - 2) + 1);
+    return index.toString();
+  };
+
+  randomInterval = (max = 3000, min = 200) => {
+    return Math.random() * (max - min) + min;
+  };
+
+  toggleGaudy = () => {
+    this.glitchLetter(true)
+    this.glitchLetter(true)
+    this.glitchLetter(true)
+    this.setState({ isGaudy: this.state.isGaudy ? false : true });
+  };
 }
+
+  const letterOptions = {
+    0: ["A", "^", "@", "ˆ"],
+    1: ["B", "3", "ß"],
+    2: ["S", "5", "§", "š"],
+    3: ["T", "†", "#"],
+    4: ["R", "®", "&"],
+    5: ["A", "^", "@", "ˆ"],
+    6: ["C", "©", "[", "{", "(", "¢"],
+    7: ["T", "†", "#"],
+    8: ["I", "!", "1", "|", "/", "¦"],
+    9: ["F", "=", "±", "?", "#", "ƒ", "‡"],
+    10: ["Y", "Ÿ", "µ", "¥"],
+  };
 
 export default App;
  
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,117 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import './Header.scss'
-import './slider.scss'
+import './toggle.scss'
 
-    const letterOptions = {
-      0: ["A", "^", "@", "ˆ"],
-      1: ["B", "3", "ß"],
-      2: ["S", "5", "§", "š"],
-      3: ["T", "†", "#"],
-      4: ["R", "®", "&"],
-      5: ["A", "^", "@", "ˆ"],
-      6: ["C", "©", "[", "{", "(", "¢"],
-      7: ["T", "†", "#"],
-      8: ["I", "!", "1", "|", "/", "¦"],
-      9: ["F", "=", "±", "?", "#", "ƒ", "‡"],
-      10: ["Y", "Ÿ", "µ", "¥"],
-    };
-
-class Header extends Component {
-  constructor() {
-    super()
-      this.state = {
-        title: [],
-        gaudyMode: true
-      }
-  }
-
-  componentDidMount() {
-    this.loadTitle()
-  }
-
-  randomIndex = (limit) => {
-    let index = Math.floor(Math.random() * (limit - 2) + 1) 
-    return index.toString()
-  }
-
-  randomInterval = (max = 3000, min = 200) => {
-    return Math.random() * (max - min) + min
-  }
-m
-  loadTitle = () => {
-    for (let i = 0; i < 11; i++){
-      this.glitchLetter(false, i)
-        for (let j = 0; j < this.randomIndex(4); j++) {
-          setTimeout(() => {
-            this.glitchLetter(false, i)
-            this.glitchLetter(true, i)
-          }, this.randomInterval(j === 0 ? 1000 : undefined))
-        }
-    }
-  }
-
-  glitchLetter = (fix, index = this.randomIndex(11)) => {
-    const glitchTitle = this.state.title
-    const letterLimit = () => {
-      return letterOptions[index.toString()].length
-    }
-    glitchTitle[parseInt(index)] =
-      letterOptions[index.toString()][this.randomIndex(letterLimit())];
-    this.setState({ title: glitchTitle })
-    if (fix) this.correctLetter(index)
-  }
-
-  correctLetter = (index) => {
-    setTimeout(() => {
-      const fixTitle = this.state.title
-      fixTitle[parseInt(index)] = letterOptions[index][0]
-      this.setState({ title: fixTitle})
-    }, this.randomInterval())
-  }
-
-  toggleGaudy = () => {
-    this.setState({ gaudyMode: this.state.gaudyMode ? false : true })
-  }
-
-  render() {
+const Header = ({ title, isGaudy, toggleGaudy, refresh, glitch}) => {
     return (
-      <header role="heading" className={!this.state.gaudyMode ? "mono" : ""}>
-        <div onClick={this.glitchLetter}>
-          <span role="banner" className="title" tabIndex="0">
-            {this.state.title.join("")}
+      <header role="heading" className={isGaudy ? "" : "mono"}>
+        <div>
+          <span role="banner" onClick={glitch} className="title" tabIndex="0">
+            {title.join("")}
           </span>
         </div>
-          <label className="switch">
-            <input type="checkbox" />
-            <span className="slider round" onClick={this.toggleGaudy}></span>
-          </label>
-          <span className="toggleLabel">
-            gaudy mode
-          </span>
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round" onClick={toggleGaudy}></span>
+        </label>
+        <span className="toggleLabel">gaudy mode</span>
         <nav>
           <NavLink
             exact
             to="/"
             className="inactive-link"
             activeClassName="active-link"
-            onClick={this.glitchLetter}
+            onClick={glitch}
           >
             Home
           </NavLink>
-          <button title="About Abstractify" onClick={this.glitchLetter}>
+          <button
+            title="About Abstractify"
+            onClick={() => {
+              glitch(true)
+            }}
+          >
             About
           </button>
-          <button 
-            title="Refresh image set" 
+          <button
+            title="Refresh image set"
             onClick={() => {
-              this.props.refresh()
-              this.glitchLetter() 
+              refresh();
+              glitch(true);
             }}
           >
             Refresh
           </button>
-          <button title="Save this image set" onClick={this.glitchLetter}>
+          <button
+            title="Save this image set"
+            onClick={() => {
+              glitch(true);
+            }}
+          >
             Save
           </button>
           <NavLink
@@ -119,14 +56,15 @@ m
             className="inactive-link"
             title="See your saved image sets"
             activeClassName="active-link"
-            onClick={this.glitchLetter}
+            onClick={() => {
+              glitch(true);
+            }}
           >
             Your Sets
           </NavLink>
         </nav>
       </header>
     );
-  }
 }
 
 export default Header
